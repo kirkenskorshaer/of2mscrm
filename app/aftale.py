945baf4519c5b511c7f4c5aed67f47f7517d8120
kkkSvend <svend.l@kirkenskorshaer.dk>
2017-11-06 16:35:54 +0100

    validating aftale against schema

--- app/aftale.py
+++ app/aftale.py
@@ -1,5 +1,6 @@
-from flask import jsonify
+from flask import jsonify, request
 from app import app
+from .schema import schema_validation, aftale_post_schema, aftale_put_schema
 
 
 @app.route('/aftale', methods=['POST'])
@@ -10,6 +11,10 @@ def create_aftale():
 		'error': None
 	}
 
+	error, aftale_request = schema_validation.validate_json_schema(aftale_post_schema.get_schema(), request)
+	if error is not None:
+		aftale_response['error'] = error
+		return jsonify(aftale_response)
 	return jsonify(aftale_response)
 
 
@@ -18,5 +23,11 @@ def update_aftale():
 	aftale_response = {
 		'error': None
 	}
+
+	error, aftale_request = schema_validation.validate_json_schema(aftale_put_schema.get_schema(), request)
+
+	if error is not None:
+		aftale_response['error'] = error
+		return jsonify(aftale_response)
 
 	return jsonify(aftale_response)
